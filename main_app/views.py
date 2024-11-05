fbf93929ae3a4e86fa72de440be9e84fd95c9ff0
Trutrucat <167733097+Trutrucat@users.noreply.github.com>
2024-11-04 17:46:16 -0800

    lectureactivity

---
+++ main_app/views.py
@@ -0,0 +1,17 @@
+# main_app/views.py
+
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+# Import HttpResponse to send text-based responses
+from django.http import HttpResponse
+
+# Define the home view function
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+
